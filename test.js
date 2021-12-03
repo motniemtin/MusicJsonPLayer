e87c62ee3193c24117d0f02ef7ee0252a6393e40
Lam Nguyen <motniemtin@gmail.com>
2021-12-03 10:08:18 +0700

    update

--- test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-const jsonFile = "./data.final.json";
-const MusicJsonPlayer = require("./src/index");
-const player = new MusicJsonPlayer('title', 'artist', jsonFile);
-player.start();
