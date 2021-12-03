a9cebe7864ce3b4529fa482add70866ab38c34af
Lam Nguyen <motniemtin@gmail.com>
2021-12-03 09:12:53 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,64 @@
 // musicjsonplayer.js
 'use strict';
+class MusicJsonPlayer {    
+    constructor(title, artist, jsonFile, jsonLyric = null) {
+        this.json = require(jsonFile);
+        if(jsonLyric == null){
+            this.jsonLyric = null;
+        }else{
+            this.jsonLyric = require(jsonLyric);
+        }        
+        this.title = title;
+        this.artist = artist;
+        this.interval = null;
+        this.ticks = -1;
+    }
+    start() {
+        this.interval = window.setInterval(() => this.runTimer(), 100);
+    }
+    restart() {
+        this.stop();
+        this.start();
+    }
+    stop() {
+        window.clearInterval(this.interval);
+        this.ticks = 0;
+    }
+    runTimer() {
+        this.ticks += 1;
+        if(typeof this.json[this.ticks]!="undefined"){
+            this.json[this.ticks].map((id) => {
+                const event = this.json[this.ticks][id];                
+                var type = event['type'];
+                switch(type) {
+                    case 'notes':
+                        this.NoteOnHander(event['data']);
+                        break;
+                    case 'lyrics':
+                        
+                        break;
+                    case 'harmonys':
+                        
+                        break;
+                }
+            })
+            
+        }
+        if(this.jsonLyric!=null){
+            if(typeof this.jsonLyric[this.ticks]!="undefined"){
+                this.NoteOnHander(event['data']);
+            }
+        }
+    }
+    addNoteOnListener(NoteOnHander) {
+        this.NoteOnHander = NoteOnHander;
+    };
 
-module.exports = class MusicJsonPlayer {
-   constructor(firstName, lastName) {
-       this.firstName = firstName;
-       this.lastName = lastName;
-   }
-   display() {
-       console.log(this.firstName + " " + this.lastName);
-   }
+    addNoteOffListener(NoteOffHander) {
+        this.NoteOffHander = NoteOffHander;
+    };
+    addHarmonyListener(HarmonyHander) {
+        this.HarmonyHander = HarmonyHander;
+    };
 }
+module.exports = MusicJsonPlayer;
--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-# MusicJsonPlayer
-```
 'use strict';
 const jsonFile = "./data.final.json";
 const MusicJsonPlayer = require("./src/index");
 const player = new MusicJsonPlayer('title', 'artist', jsonFile);
 player.start();
-```
